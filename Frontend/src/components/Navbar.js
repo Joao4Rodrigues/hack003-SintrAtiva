62dab70107abb571214bf7da944f67c7c84e2468
InesBCoelho <ines-coelho@hotmail.com>
2021-07-22 10:57:24 +0100

    navbar

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -11,10 +11,10 @@ function NavBar() {
     return (
         <>
             <img height='100px' src='/SintrAtiva.png' />
-            <div>
-                <button><MdFavorite size ={35}/></button>
-                <Link to="/"><button className='homeButton'><BsFillHouseDoorFill className='homeIcon' /><br />Homepage</button></Link>
-                <button><MdEvent size ={35}/></button>
+            <div className='homeBar'>
+                <button><MdFavorite size ={35}/>Favoritos</button>
+                <Link to="/"><button ><BsFillHouseDoorFill  size ={35}/><br />Homepage</button></Link>
+                <button><MdEvent size ={35}/>Eventos</button>
                 
             </div>
         </>
